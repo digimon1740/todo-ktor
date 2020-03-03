7863383cab14d8e88f9c11437f98719cacdb834a
digimon <digimon.1740@gmail.com>
2020-03-04 00:26:00 +0900

    라우터 분리

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,21 @@
+import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.Application
-import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
-import io.ktor.http.ContentType
-import io.ktor.response.respondText
+import io.ktor.jackson.jackson
 import io.ktor.routing.Routing
-import io.ktor.routing.get
 
 fun Application.main() {
     install(DefaultHeaders)
     install(CallLogging)
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
     install(Routing) {
-        root()
-        hello()
-    }
-}
-
-// Extracted route
-fun Routing.root() {
-    get("/") {
-        call.respondText("Hello World!", ContentType.Text.Html)
-    }
-}
-
-fun Routing.hello() {
-    get("/hello") {
-        call.respondText("My Example Blog2", ContentType.Text.Html)
+        todo()
     }
 }
---
+++ src/main/kotlin/TodoRouter.kt
@@ -0,0 +1,35 @@
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.request.receive
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.delete
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.put
+import io.ktor.routing.route
+
+fun Routing.todo() {
+    route("todos") {
+        get {
+            val startsAt = call.request.queryParameters["starts_at"].toString()
+            val beginsAt = call.request.queryParameters["begins_at"].toString()
+
+            call.respondText("Hello Todos ", ContentType.Text.Html)
+        }
+        get("/{id}") {
+            val id = call.parameters["id"]
+            call.respondText("Hello Todos $id", ContentType.Text.Html)
+        }
+        post {
+            val body = call.receive<Map<String, Any>>()
+
+        }
+        put() {
+
+        }
+        delete() {
+
+        }
+    }
+}
